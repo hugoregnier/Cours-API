e64a069fc5d2e495566dc452bdaf21e5d27b173c
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-07 17:08:04 +0200

    fintion et commentaires

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const Thing = require('./models/thing');
 
 
 const app = express();
 
+// connexion a la bdd mongo.db.atlas 
 mongoose.connect('mongodb+srv://hugoregnier:hugo21@cluster0.sumrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+// ajout des cross origin pour autoriser
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+// prends la requête du formulaire post 
 app.post('/api/stuff', (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({
@@ -34,7 +35,30 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
+// modification le l'objet
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// suppression le l'objet
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// url de l'id d'un seul produit (quand on clique sur le produit on arrive sur la page produit)
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+
+// affiche tout les produits 
+app.get('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
