4ef29fe8447ee43fb684609f8886a9faaf5a96c3
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-14 12:08:45 +0200

    add auth middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth')
 
 // prends la requête du formulaire post 
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, stuffCtrl.createThing);
 
 // modification le l'objet
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
 
 // suppression le l'objet
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 // url de l'id d'un seul produit (quand on clique sur le produit on arrive sur la page produit)
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
 
 
 // affiche tout les produits 
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
